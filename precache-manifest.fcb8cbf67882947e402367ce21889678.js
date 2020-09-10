3c0fd231d7b82ea0b55e4aac7c6e24bca4cad5ab
cainster <1caindo@gmail.com>
2020-09-09 18:53:00 -0700

    Updates

---
+++ precache-manifest.fcb8cbf67882947e402367ce21889678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d056985fb26ef782714383f45906bb",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "500a32dec537c1c78f97",
+    "url": "/test/static/css/main.7b507010.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/test/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500a32dec537c1c78f97",
+    "url": "/test/static/js/main.6d1e663e.chunk.js"
+  },
+  {
+    "revision": "0fedd88b05a2847d5203",
+    "url": "/test/static/js/runtime-main.e830598a.js"
+  }
+]);
